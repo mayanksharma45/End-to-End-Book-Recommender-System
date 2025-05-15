d868d787e1fb86d9afb8d39713288bdade3d341b
mayanksharma45 <mayanksharma111465r@example.com>
2025-05-15 22:50:18 +0530

    Updated

--- books_recommender/components/stage_00_data_ingestion.py
+++ books_recommender/components/stage_00_data_ingestion.py
@@ -31,12 +31,12 @@ class DataIngestion:
             dataset_url = self.data_ingestion_config.dataset_download_url
             zip_download_dir = self.data_ingestion_config.raw_data_dir
             os.makedirs(zip_download_dir, exist_ok=True)
-            data_file_name = os.path.basename(dataset_url)
+            data_file_name = os.path.basename(dataset_url)    # if the URL is https://domain.com/data.zip, this line returns "data.zip"
             zip_file_path = os.path.join(zip_download_dir, data_file_name)
             logging.info(f"Downloading data from {dataset_url} into file {zip_file_path}")
-            urllib.request.urlretrieve(dataset_url,zip_file_path)
+            urllib.request.urlretrieve(dataset_url,zip_file_path)  # This downloads the file from the dataset_url and saves it at zip_file_path
             logging.info(f"Downloaded data from {dataset_url} into file {zip_file_path}")
-            return zip_file_path
+            return zip_file_path   # unzipping it later.
 
         except Exception as e:
             raise AppException(e, sys) from e
