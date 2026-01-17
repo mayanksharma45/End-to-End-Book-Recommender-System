5678c9934e9e5f26ca152949e961e116bf38ba17
mayank-sharma-ai <mayank.sharma@intellimark.ai>
2026-01-17 11:57:33 +0530

    updated

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ import sys
 import pickle
 import streamlit as st
 import numpy as np
+from pathlib import Path
 from books_recommender.logger.log import logging
 from books_recommender.config.configuration import AppConfiguration
 from books_recommender.pipeline.training_pipeline import TrainingPipeline
 from books_recommender.exception.exception_handler import AppException
+
+BASE_DIR = Path(__file__).resolve().parent
+
+ARTIFACTS_DIR = BASE_DIR / "artifacts"
+TRAINED_MODEL_PATH = ARTIFACTS_DIR / "trained_model" / "model.pkl"
+BOOK_PIVOT_PATH = ARTIFACTS_DIR / "serialized_objects" / "book_pivot.pkl"
+FINAL_RATING_PATH = ARTIFACTS_DIR / "serialized_objects" / "final_rating.pkl"
+
+TEMPLATES_DIR = BASE_DIR / "templates"
+BOOK_NAMES_PATH = TEMPLATES_DIR / "book_names.pkl"
+
 
 
 class Recommendation:
@@ -17,50 +29,60 @@ class Recommendation:
             raise AppException(e, sys) from e
 
 
-    def fetch_poster(self,suggestion):
+    def fetch_poster(self, suggestion):
         try:
             book_name = []
             ids_index = []
             poster_url = []
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
-            final_rating =  pickle.load(open(self.recommendation_config.final_rating_serialized_objects,'rb'))
+
+            book_pivot = pickle.load(open(BOOK_PIVOT_PATH, "rb"))
+            final_rating = pickle.load(open(FINAL_RATING_PATH, "rb"))
 
             for book_id in suggestion:
                 book_name.append(book_pivot.index[book_id])
 
-            for name in book_name[0]: 
-                ids = np.where(final_rating['title'] == name)[0][0]
+            for name in book_name[0]:
+                ids = np.where(final_rating["title"] == name)[0][0]
                 ids_index.append(ids)
 
             for idx in ids_index:
-                url = final_rating.iloc[idx]['image_url']
+                url = final_rating.iloc[idx]["image_url"]
                 poster_url.append(url)
 
             return poster_url
-        
+
         except Exception as e:
             raise AppException(e, sys) from e
+
         
 
 
-    def recommend_book(self,book_name):
+    def recommend_book(self, book_name):
         try:
             books_list = []
-            model = pickle.load(open(self.recommendation_config.trained_model_path,'rb'))
-            book_pivot =  pickle.load(open(self.recommendation_config.book_pivot_serialized_objects,'rb'))
+
+            model = pickle.load(open(TRAINED_MODEL_PATH, "rb"))
+            book_pivot = pickle.load(open(BOOK_PIVOT_PATH, "rb"))
+
             book_id = np.where(book_pivot.index == book_name)[0][0]
-            distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
+
+            distance, suggestion = model.kneighbors(
+                book_pivot.iloc[book_id, :].values.reshape(1, -1),
+                n_neighbors=6
+            )
 
             poster_url = self.fetch_poster(suggestion)
-            
+
             for i in range(len(suggestion)):
-                    books = book_pivot.index[suggestion[i]]
-                    for j in books:
-                        books_list.append(j)
-            return books_list , poster_url   
-        
+                books = book_pivot.index[suggestion[i]]
+                for j in books:
+                    books_list.append(j)
+
+            return books_list, poster_url
+
         except Exception as e:
             raise AppException(e, sys) from e
+
 
 
     def train_engine(self):
@@ -108,7 +130,7 @@ if __name__ == "__main__":
     if st.button('Train Recommender System'):
         obj.train_engine()
 
-    book_names = pickle.load(open(os.path.join('templates','book_names.pkl') ,'rb'))
+    book_names = pickle.load(open(BOOK_NAMES_PATH, "rb"))
     selected_book = st.selectbox(
         "Type or select a book from the dropdown",
         book_names)
